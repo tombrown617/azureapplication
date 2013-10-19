60bda672a01fd43b67cbe575e9ccefa1b63cc0e3
Tom Brown <tom@tombrown.it>
2013-10-19 16:53:26 +0100

    index.php updated to include companyname

--- index.php
+++ index.php
@@ -21,6 +21,7 @@
 <form method="post" action="index.php" enctype="multipart/form-data" >
       Name  <input type="text" name="name" id="name"/></br>
       Email <input type="text" name="email" id="email"/></br>
+      Company Name <input type="text" name="companyname" id="companyname"/></br>
       <input type="submit" name="submit" value="Submit" />
 </form>
 <?php
@@ -45,13 +46,15 @@
         $name = $_POST['name'];
         $email = $_POST['email'];
         $date = date("Y-m-d");
+        $companyname = $_POST['companyname'];
         // Insert data
         $sql_insert = "INSERT INTO registration_tbl (name, email, date) 
-                   VALUES (?,?,?)";
+                   VALUES (?,?,?,?)";
         $stmt = $conn->prepare($sql_insert);
         $stmt->bindValue(1, $name);
         $stmt->bindValue(2, $email);
         $stmt->bindValue(3, $date);
+        $stmt->bindValue(4, $companyname);
         $stmt->execute();
     }
     catch(Exception $e) {
@@ -68,10 +71,12 @@
         echo "<table>";
         echo "<tr><th>Name</th>";
         echo "<th>Email</th>";
+        echo "<th>Company Name</th>;   
         echo "<th>Date</th></tr>";
         foreach($registrants as $registrant) {
             echo "<tr><td>".$registrant['name']."</td>";
             echo "<td>".$registrant['email']."</td>";
+            echo "<td>".$registrant['companyname']"</td>";
             echo "<td>".$registrant['date']."</td></tr>";
         }
         echo "</table>";
