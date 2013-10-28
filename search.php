2d83f88053f82161662aa07df9047a501973c90d
tombrown617 <tombrown617@sky.com>
2013-10-28 22:42:18 +0000

    Update search.php

--- search.php
+++ search.php
@@ -65,7 +65,8 @@
                 try {
                         //Copy POST data to variables and perform relevant SQL SELECT
                         $searchtype = $_POST['searchtype'];
-                $searchterm = $_POST['searchterm'];
+                	$searchterm = $_POST['searchterm'];
+                	echo "<p>".$searchtype.":".$searchterm"</p>"
                         $sql_select = "SELECT * FROM registration_tbl WHERE ? LIKE '%?%'";
                         $stmt = $conn->prepare($sql_select);
                         $stmt->bindValue(1, $searchtype);
